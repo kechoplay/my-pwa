4ebad55f2145b261c809acb204410b11477bd9b7
tungks <tungks@watermelon.vn>
2025-03-03 16:51:30 +0700

    add

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/public/firebase-messaging-sw.js')
+    navigator.serviceWorker.register('/firebase-messaging-sw.js')
         .then(registration => {
             console.log('Service Worker registered:', registration);
         })
